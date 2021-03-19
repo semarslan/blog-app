eb9703ff5eb29fcb7c15a9e242984ea175730530
semarslan <94.sema.arslan@gmail.com>
2021-03-19 15:09:33 +0300

    redux devTools and firebase added.

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+import 'firebase/database';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDLBJDIKl6YTw3l0Zc5CDwXvXctrhRGp14",
+    authDomain: "blog-app-fff64.firebaseapp.com",
+    projectId: "blog-app-fff64",
+    storageBucket: "blog-app-fff64.appspot.com",
+    messagingSenderId: "120934120919",
+    appId: "1:120934120919:web:860169cbebf7f63c991f92"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,7 +7,8 @@ export default () => {
         combineReducers({
             blogs: blogReducer,
             auth: authReducer
-        })
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
     return store;
 }
