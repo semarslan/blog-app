568f6ef7ef4b500d3c16d11bc1085cc82fde0f87
semarslan <94.sema.arslan@gmail.com>
2021-03-19 10:45:29 +0300

    add Redux

---
+++ src/actions/blogs.js
@@ -0,0 +1,25 @@
+import {v4 as uuid} from 'uuid';
+
+export const addBlog = ({title = '', description = '', dateAdded = 0}) => ({
+    type: "ADD_BLOG",
+    blog: {
+        id: uuid(),
+        title: title,
+        description: description,
+        dateAdded: dateAdded
+    }
+})
+
+export const removeBlog = ({id}) => (
+    {
+        type: "REMOVE_BLOG",
+        id: id
+    }
+)
+
+export const editBlog = (id, updateData) => ({
+    type: "EDIT_BLOG",
+    id: id,
+    updateData: updateData
+
+})
---
+++ src/store/configureStore.js
@@ -0,0 +1,14 @@
+import {combineReducers, createStore} from "redux";
+import blogReducer from "../reducers/blogs";
+import authReducer from "../reducers/auth";
+
+export default () => {
+    const store = createStore(
+        combineReducers({
+            blogs: blogReducer,
+            auth: authReducer
+        })
+    );
+    return store;
+}
+
