2a0e39e9c5c032ec8e9386ff3bfe708458a4fd54
semarslan <94.sema.arslan@gmail.com>
2021-03-19 11:26:40 +0300

    component redux

--- src/components/Blog/BlogList.js
+++ src/components/Blog/BlogList.js
@@ -1,14 +1,17 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {connect} from "react-redux";
 
-const BlogList = () => {
+const BlogList = (props) => {
     return (
         <div>
-            <h1>Blog List</h1>
-            <Link to={"/blogs/1"}>Blog 1</Link>
-            <Link to={"/blogs/2"}>Blog 2</Link>
+            {props.blogs.length}
         </div>
     )
 }
 
-export default BlogList;
+const mapStateToProps = (state) => {
+    return {
+        blogs: state.blogs
+    }
+}
+export default connect(mapStateToProps)(BlogList);
